1ca405241599346e222ee9644f241258e5db7cfe
kopicee <epicfishball@gmail.com>
2023-07-30 23:59:46 +0800

    Restructure unit tests using plain asserts instead of table-driven tests

--- app/ingest/test_normalize.py
+++ app/ingest/test_normalize.py
@@ -2,41 +2,31 @@ from app.ingest.normalize import *
 
 
 def test_split_camel_case():
-    testcases = {
-        'CamelCase': ['Camel', 'Case'],
-        'camelCase': ['camel', 'Case'],
-        'camelCase123': ['camel', 'Case123'],
-    }
-    for input, expect in testcases.items():
-        assert Strings.split_camel_case(input) == expect
+    assert Strings.split_camel_case('CamelCase') ==  ['Camel', 'Case']
+    assert Strings.split_camel_case('camelCase') == ['camel', 'Case']
+    assert Strings.split_camel_case('camelCase123') == ['camel', 'Case123']
 
 
 def test_country_from_iso():
-    testcases = {
-        ('SG', 'en', 'US'): 'Singapore',
-        ('JP', 'en', 'US'): 'Japan',
-    }
-    for input, expect in testcases.items():
-        assert Addresses.country_from_iso(*input) == expect
+    assert Addresses.country_from_iso('SG') == 'Singapore', 'happy case with default args'
+    assert Addresses.country_from_iso('JP', 'en', 'US') == 'Japan', 'happy case with explicit args'
 
 
 def test_to_normalized_tags():
-    testcases = {
-        ('tv', 'iron'): ['iron', 'tv'],  # sorting
-        ('Aircon', 'Tub'): ['aircon', 'tub'],  # lowercasing
-        ('HairDryer', 'DryCleaning'): ['dry cleaning', 'hair dryer'],  # camel case
-        ('WiFi',): ['wifi'],  # wifi edge case
-        ('aircon', 'aircon'): ['aircon']  # de-duplication
-    }
-    for input, expect in testcases.items():
-        assert Amenities.to_normalized_tags(input) == expect
+    f = Amenities.to_normalized_tags
+    assert f('tv', 'iron') == ['iron', 'tv'], 'should return in sorted order'
+    assert f('Aircon', 'Tub') == ['aircon', 'tub'], 'should return lowercase'
+    assert f('HairDryer', 'DryCleaning') == ['dry cleaning', 'hair dryer'], 'should split camel case'
+    assert f('WiFi') == ['wifi'], 'should handle wifi edge case correctly'
+    assert f('aircon', 'aircon') == ['aircon'], 'should remove duplicates'
+    assert f('dry cleaning', 'hair dryer') == ['dry cleaning', 'hair dryer'], 'should noop if format already correct'
 
 
 def test_lat_long_precision():
-    assert lat_long_precision(1.23) == 2
-    assert lat_long_precision(1) == -1
-    assert lat_long_precision(Consts.EMPTY_LATITUDE) == -1
-    assert lat_long_precision(Consts.EMPTY_LONGITUDE) == -1
+    assert lat_long_precision(1.23) == 2, 'should return number of decimal places'
+    assert lat_long_precision(1) == -1, 'non-float should return -1'
+    assert lat_long_precision(Consts.EMPTY_LATITUDE) == -1, 'EMPTY_LATITUDE should return -1'
+    assert lat_long_precision(Consts.EMPTY_LONGITUDE) == -1, 'EMPTY_LONGITUDE should return -1'
 
 
 def test_argmax():
