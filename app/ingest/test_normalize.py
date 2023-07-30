fe8469bbe0f656e814b81606dd568a79af81721e
kopicee <epicfishball@gmail.com>
2023-07-30 23:23:13 +0800

    Adding more tests

--- app/ingest/test_normalize.py
+++ app/ingest/test_normalize.py
@@ -25,8 +25,22 @@ def test_to_normalized_tags():
         ('tv', 'iron'): ['iron', 'tv'],  # sorting
         ('Aircon', 'Tub'): ['aircon', 'tub'],  # lowercasing
         ('HairDryer', 'DryCleaning'): ['dry cleaning', 'hair dryer'],  # camel case
-        ('WiFi',): ['wifi'],  # wifi edge case & de-duplication
+        ('WiFi',): ['wifi'],  # wifi edge case
+        ('aircon', 'aircon'): ['aircon']  # de-duplication
     }
     for input, expect in testcases.items():
         assert Amenities.to_normalized_tags(input) == expect
 
+
+def test_lat_long_precision():
+    assert lat_long_precision(1.23) == 2
+    assert lat_long_precision(1) == -1
+    assert lat_long_precision(Consts.EMPTY_LATITUDE) == -1
+    assert lat_long_precision(Consts.EMPTY_LONGITUDE) == -1
+
+
+def test_argmax():
+    assert argmax(1, 5) == 5
+    assert argmax('a', 'abcde', len) == 'abcde'
+    assert argmax('a', 'b', len) == 'a'
+    assert argmax(1.2, 1.234, lat_long_precision) == 1.234
